f26595e4594e155c10080657992a12395e704f19
Vinícius de Paula <manoseilacara123@gmail.com>
2025-07-25 09:41:30 -0300

    initial commit

---
+++ src/test/java/com/nubank/desafio_backend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nubank.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
